2bda61fa367b509dcb13d691a85ea797f0689df1
walhalla4 <ktksg8@gmail.com>
2024-07-28 10:45:19 +0900

    Init

---
+++ src/main/java/com/flow/payment/FlowPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.flow.payment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flow/payment/FlowPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flow.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
